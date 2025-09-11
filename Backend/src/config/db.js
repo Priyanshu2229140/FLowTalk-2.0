e15782744e40ad55915f67ae262e5f2003640525
Priyanshu2229140 <priyanshujana07@gmail.com>
2025-09-11 05:44:11 +0530

    Add database connection and environment variables setup

---
+++ Backend/src/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { ENV } from "./env.js";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(ENV.MONGO_URI);
+    console.log("DB connected successfully", conn.connection.host);
+  } catch (error) {
+    console.log("Error in DB connection", error);
+    process.exit(1);
+  }
+};
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+console.log(ENV.MONGO_URI);
 app.listen(ENV.PORT, () => {
-  console.log(ENV.MONGO_URI);
   console.log("Server is running on port ", ENV.PORT);
+  connectDB();
 });
